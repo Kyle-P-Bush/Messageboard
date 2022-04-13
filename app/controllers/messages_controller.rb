ae099045dba5f6aa9d2e59274bd8751b0aebd8ee
kpbush1 <kpbush1@uno.edu>
2022-04-13 16:06:10 -0500

    Formatted signup, login, and forgot password page

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /messages or /messages.json
   def index
